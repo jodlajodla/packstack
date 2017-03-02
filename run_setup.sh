9d8a037180197db4b19c8b365e33a20edffe6b95
Jan Sušnik <jodlajodla@gmail.com>
2017-03-02 18:13:34 +0100

    Fixed setup script to install Ironic UI to Horizon and restart networking in case of bridging

--- run_setup.sh
+++ run_setup.sh
@@ -57,9 +57,14 @@ $SUDO cp -r packstack/puppet/modules/packstack /usr/share/openstack-puppet/modul
 
 # Check which type of installation was chosen and do workaround with Puppetfile in case of Ironic
 if [ $INCLUDE_IRONIC -eq 0 ]; then
+	# Install ironic-ui component for Horizon
+	$SUDO yum install openstack-ironic-ui
 	if [ ! -z "$BRIDGE_INTERFACE" ]; then
 		# Install Packstack & Ironic with bridged interface
 		$SUDO packstack --os-ironic-install=y --nagios-install=n --provision-demo=n --os-neutron-ovs-bridge-mappings="$EXTERNAL_NETWORK":br-ex --os-neutron-ovs-bridge-interfaces=br-ex:"$BRIDGE_INTERFACE" --allinone
+		# Wait a bit and restart network to get connection back if bridging settings stopped interfaces
+		sleep 30
+		$SUDO systemctl restart network
 	else
 		# Install only Packstack & Ironic with existing network configuration
 		$SUDO packstack --os-ironic-install=y --nagios-install=n --provision-demo=n --allinone
